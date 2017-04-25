f3a29b15afe42dd18963e0476ec4d84fe6d1c2c8
Kelcy Newton <kelcynewton@gmail.com>
2017-04-25 16:00:16 -0400

    adding access-control-allow-origin

--- js/contact.js
+++ js/contact.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://kelcynewton.com/js/submit.php",
+                url: "submit.php",
                 type: "POST",
                 data: {
                     name: name,
